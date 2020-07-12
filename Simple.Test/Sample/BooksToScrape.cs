259c1b583d64454001e9161e77554d20616254a1
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-07-12 18:01:10 -0300

    Update BooksToScrape.cs
    Simplifcation and added Product Details Table

--- Simple.Test/Sample/BooksToScrape.cs
+++ Simple.Test/Sample/BooksToScrape.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Xml.Linq;
 using System.Xml.XPath;
 using System.Globalization;
+using Net.RafaelEstevam.Spider.Extensions;
+using System.Data;
 
 namespace Net.RafaelEstevam.Spider.Test.Sample
 {
@@ -13,24 +15,15 @@ namespace Net.RafaelEstevam.Spider.Test.Sample
     /// </summary>
     public class BooksToScrape
     {
-        static List<BookData> lstBooks;
+        static List<BookData> lstBooks = new List<BookData>();
         public static void run()
         {
-            lstBooks = new List<BookData>();
-            // Spider initialization
             var spider = new SimpleSpider("BooksToScrape", new Uri("http://books.toscrape.com/"));
-            Console.WriteLine($"SPider at: {spider.Configuration.SpiderDirectory}");
             // callbacks
             spider.FetchCompleted += fetchCompleted_links; // callback to gather links
             spider.FetchCompleted += fetchCompleted_items; // callback to gather items
-
-            spider.ShouldFetch += Spider_ShouldFetch;
-
-            // configuration
-            spider.Configuration.DownloadDelay = 5000; // be kind to others
-            // add first page
-            spider.AddPage(spider.BaseUri, spider.BaseUri);
-            // execute
+            spider.ShouldFetch += Spider_ShouldFetch;      // Ignore some pages
+            // execute from first page
             spider.Execute();
 
             // List all books
@@ -42,8 +35,7 @@ namespace Net.RafaelEstevam.Spider.Test.Sample
 
         private static void Spider_ShouldFetch(object Sender, ShouldFetchEventArgs args)
         {
-            // All reviews links returns 404-Not Found
-            // ignore, them
+            // All reviews links returns 404-Not Found, ignore them
             if (args.Link.Uri.ToString().Contains("/reviews/")) args.Cancel = true;
         }
         private static void fetchCompleted_links(object Sender, FetchCompleteEventArgs args)
@@ -55,27 +47,29 @@ namespace Net.RafaelEstevam.Spider.Test.Sample
         }
         private static void fetchCompleted_items(object Sender, FetchCompleteEventArgs args)
         {
-            string strUrl = args.Link.ToString();
             // ignore all pages except the catalogue
             if (!args.Link.ToString().Contains("/catalogue/")) return;
 
             // collect book data
             var articleProd = args.XElement.XPathSelectElement("//article[@class=\"product_page\"]");
             if (articleProd == null) return; // not a book
-
+            // Book info
             var sTitle = articleProd.XPathSelectElement("//h1").Value;
             var sPrice = articleProd.XPathSelectElement("//p[@class=\"price_color\"]").Value;
             var sStock = articleProd.XPathSelectElement("//p[@class=\"instock availability\"]").Value.Trim();
             var sDesc = articleProd.XPathSelectElement("p")?.Value; // books can be descriptonless
-
+            // convert price to Decimal
             decimal.TryParse(sPrice, NumberStyles.Currency, new CultureInfo("en-GB", false), out decimal price);
-
+            // Product info table
+            var table = args.XElement.GetAllTables().First();
+            
             lstBooks.Add(new BookData()
             {
                 Title = sTitle,
                 Price = price,
                 Description = sDesc,
-                StockInfo = sStock
+                StockInfo = sStock,
+                PrductInfoTable = table,
             });
         }
 
@@ -85,6 +79,7 @@ namespace Net.RafaelEstevam.Spider.Test.Sample
             public decimal Price { get; internal set; }
             public string Description { get; internal set; }
             public string StockInfo { get; internal set; }
+            public DataTable PrductInfoTable { get; internal set; }
         }
     }
 }
