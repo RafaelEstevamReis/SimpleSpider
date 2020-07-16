aa667ebe550a1430db2e0d6d5043865686285d20
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-07-15 23:57:40 -0300

    Update BooksToScrape.cs
    Simplification

--- Simple.Test/Sample/BooksToScrape.cs
+++ Simple.Test/Sample/BooksToScrape.cs
@@ -20,30 +20,30 @@ namespace Net.RafaelEstevam.Spider.Test.Sample
         {
             var spider = new SimpleSpider("BooksToScrape", new Uri("http://books.toscrape.com/"));
             // callbacks
-            spider.FetchCompleted += fetchCompleted_links; // callback to gather links
             spider.FetchCompleted += fetchCompleted_items; // callback to gather items
-            spider.ShouldFetch += Spider_ShouldFetch;      // Ignore some pages
+            spider.FetchCompleted += (s, a) => // callback to gather links
+            {
+                // Use a simple SubString-based split to get all "<a>" tags
+                var links = Helper.AnchorHelper.GetAnchors(a.Link.Uri, a.Html);
+                // Add the collected links to the queue
+                (s as SimpleSpider).AddPage(links, a.Link);
+            };
+            // Ignore some pages
+            spider.ShouldFetch += (s, a) =>
+            {
+                if (a.Link.Uri.ToString().Contains("/reviews/"))
+                {
+                    a.Cancel = true;
+                }
+            };
             // execute from first page
             spider.Execute();
 
             // List all books
             foreach (var b in lstBooks)
             {
-                Console.WriteLine($"{b.Price:C2} {b.Title} ");
+                Console.WriteLine($"{b.Price:C2} {b.Title}");
             }
-        }
-
-        private static void Spider_ShouldFetch(object Sender, ShouldFetchEventArgs args)
-        {
-            // All reviews links returns 404-Not Found, ignore them
-            if (args.Link.Uri.ToString().Contains("/reviews/")) args.Cancel = true;
-        }
-        private static void fetchCompleted_links(object Sender, FetchCompleteEventArgs args)
-        {
-            // Use a simple SubString-based split to get all "<a>" tags
-            var links = Helper.AnchorHelper.GetAnchors(args.Link.Uri, args.Html).ToArray();
-            // Add the collected links to the queue
-            (Sender as SimpleSpider).AddPage(links, args.Link);
         }
         private static void fetchCompleted_items(object Sender, FetchCompleteEventArgs args)
         {
