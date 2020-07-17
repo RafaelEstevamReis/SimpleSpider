28de3c92b5fbc5e27b789460ff44795898587652
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-07-16 23:31:23 -0300

    Create XmlSerializerHelper.cs
    Simple generic serializer

---
+++ Simple.Lib/Helper/XmlSerializerHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Net.RafaelEstevam.Spider.Helper
+{
+    public static class XmlSerializerHelper
+    {
+        public static void SerializeToFile<T>(T obj, string file)
+        {
+            XmlSerializer x = new XmlSerializer(typeof(T));
+            using TextWriter writer = new StreamWriter(file);
+            x.Serialize(writer, obj);
+        }
+    }
+}
