6af4db4198c031a96225658428a053c0a5775aa6
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-07-19 16:37:45 -0300

    Create JsonDeserializeParser.cs
    New parser to Deserialize content

---
+++ Simple.Lib/Parsers/JsonDeserializeParser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Net.RafaelEstevam.Spider.Interfaces;
+using Newtonsoft.Json;
+
+namespace Net.RafaelEstevam.Spider.Parsers
+{
+    public class JsonDeserializeParser<T> : IParser<T> where T : new()
+    {
+        public JsonDeserializeParser() { }
+        public JsonDeserializeParser(ParseData<T> callback)
+        {
+            ParsedData = callback;
+        }
+
+        public string[] MimeTypes => new string[] { "application/json" };
+
+        public event ParseData<T> ParsedData;
+        void IParserBase.Parse(SimpleSpider spider, FetchCompleteEventArgs fetchInfo)
+        {
+            var result = JsonConvert.DeserializeObject<T>(fetchInfo.Html);
+            ParsedData(spider, new ParserEventArgs<T>(fetchInfo, result));
+        }
+    }
+}
