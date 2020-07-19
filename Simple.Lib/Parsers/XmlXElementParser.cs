2c457875a049c9cc5bbad4e188e5d4b4b32358b8
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-07-19 20:32:36 -0300

    Update XmlXElementParser.cs
    Added easy custom encoding

--- Simple.Lib/Parsers/XmlXElementParser.cs
+++ Simple.Lib/Parsers/XmlXElementParser.cs
@@ -1,10 +1,12 @@
-﻿using System.Xml.Linq;
+﻿using System.Text;
+using System.Xml.Linq;
 using Net.RafaelEstevam.Spider.Interfaces;
 
 namespace Net.RafaelEstevam.Spider.Parsers
 {
     public class XmlXElementParser : IParser<XElement>
     {
+        public Encoding Encoding { get; set; } = Encoding.UTF8;
         public string[] MimeTypes => new string[] { "application/xml", "text/html" };
 
         public event ParseData<XElement> ParsedData;
@@ -13,7 +15,7 @@ namespace Net.RafaelEstevam.Spider.Parsers
         {
             if (ParsedData == null) return;
 
-            ParsedData(spider, new ParserEventArgs<XElement>(FetchInfo: FetchInfo, Data: XElement.Parse(FetchInfo.Html)));
+            ParsedData(spider, new ParserEventArgs<XElement>(FetchInfo, XElement.Parse(Encoding.GetString(FetchInfo.Result))));
         }
     }
 }
