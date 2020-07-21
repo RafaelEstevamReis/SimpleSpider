ff8c3c46a57cfd13294fde9dc00705808af00f9a
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-07-21 01:09:30 -0300

    Add a NullDownloader to OfflineMode

---
+++ Simple.Lib/Downloaders/NullDownloader.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Timers;
+using Net.RafaelEstevam.Spider.Interfaces;
+
+namespace Net.RafaelEstevam.Spider.Downloaders
+{
+    public class NullDownloader : IDownloader
+    {
+        public enum DiscardModeTypes
+        {
+            Ingore,
+            CompleteEmpty,
+            Fail,
+        }
+
+        private ConcurrentQueue<Link> workQueue;
+        private Timer timer;
+
+        public bool IsProcessing => false;
+        public DiscardModeTypes DiscardMode { get; set; } = DiscardModeTypes.Ingore;
+
+        public event FetchComplete FetchCompleted;
+        public event FetchFail FetchFailed;
+        public event ShouldFetch ShouldFetch;
+
+        public void Initialize(ConcurrentQueue<Link> WorkQueue, Configuration Config)
+        {
+            workQueue = WorkQueue;
+            timer = new Timer(500);
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = true;
+        }
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            workQueue.TryDequeue(out Link l);
+
+            if (ShouldFetch != null)
+            {
+                var args = new ShouldFetchEventArgs(l);
+                ShouldFetch(this, args);
+                if (args.Cancel) return; // do not fail/complete
+            }
+
+            switch (DiscardMode)
+            {
+                case DiscardModeTypes.Fail:
+                    FetchFailed?.Invoke(this,
+                                        new FetchFailEventArgs(l,
+                                                               new Exception("NullDownloader:DiscardModeTypes.Fail"),
+                                                               new System.Collections.Generic.KeyValuePair<string, string>[0]));
+                    break;
+                case DiscardModeTypes.CompleteEmpty:
+                    FetchCompleted?.Invoke(this,
+                                           new FetchCompleteEventArgs(l,
+                                                                      new byte[0],
+                                                                      new System.Collections.Generic.KeyValuePair<string, string>[0],
+                                                                      new System.Collections.Generic.KeyValuePair<string, string>[0]));
+                    break;
+                case DiscardModeTypes.Ingore:
+                default:
+                    // ignore ... 
+                    break;
+            }
+        }
+
+        public void Start()
+        {
+            timer.Enabled = true;
+        }
+
+        public void Stop()
+        {
+            timer.Enabled = false;
+        }
+    }
+}
