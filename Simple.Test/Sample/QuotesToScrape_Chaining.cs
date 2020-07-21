1080ffbffaba5e4c53ab6bfd77abecd30a083399
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-07-20 22:32:46 -0300

    Add sample QuotesToScrape_Chaining

---
+++ Simple.Test/Sample/QuotesToScrape_Chaining.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Net.RafaelEstevam.Spider.Cachers;
+using Net.RafaelEstevam.Spider.Downloaders;
+
+namespace Net.RafaelEstevam.Spider.Test.Sample
+{
+    public class QuotesToScrape_Chaining
+    {
+        public static void run()
+        {
+            var init = new InitializationParams()
+                .SetCacher(new ContentCacher())
+                .SetDownloader(new WebClientDownloader())
+                .SetSpiderStarupDirectory(null) // Default directory
+                                                // create a json parser for our QuotesObject class
+                .AddParser(new Parsers.JsonDeserializeParser<QuotesObject>(parsedResult_event))
+                .SetConfig(c => c.Enable_Caching()
+                                 .Disable_Cookies()
+                                 .Disable_AutoAnchorsLinks()
+                                 .Set_CachingNoLimit()
+                                 .Set_DownloadDelay(5000));
+
+            var spider = new SimpleSpider("QuotesToScrape", new Uri("http://quotes.toscrape.com/"), init);
+
+            // add first
+            spider.AddPage(buildPageUri(1), spider.BaseUri);
+            // execute
+            spider.Execute();
+        }
+
+        private static void parsedResult_event(object sender, Interfaces.ParserEventArgs<QuotesObject> args)
+        {
+            // add next
+            if (args.ParsedData.has_next)
+            {
+                int currPage = args.ParsedData.page;
+                ((SimpleSpider)sender).AddPage(buildPageUri(currPage + 1), args.FetchInfo.Link);
+            }
+            // Process data ...
+        }
+        public static Uri buildPageUri(int page)
+        {
+            string url = $"http://quotes.toscrape.com/api/quotes?page={ page }";
+            return new Uri(url);
+        }
+
+    }
+}
