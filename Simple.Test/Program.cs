e9d4f493655359b71df1e7d78eb9c89591ea2ba2
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-07-21 19:13:34 -0300

    Update Program.cs

--- Simple.Test/Program.cs
+++ Simple.Test/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Net.RafaelEstevam.Spider.Test.Sample;
 
 namespace Net.RafaelEstevam.Spider.Test
 {
@@ -6,9 +7,13 @@ namespace Net.RafaelEstevam.Spider.Test
     {
         static void Main(string[] args)
         {
+            // Spider Samples:
             //Sample.BooksToScrape.run();
             //Sample.QuotesToScrape_Scroll.run();
             //QuotesToScrape_Scroll_Advanced.run();
+
+            // Helper Samples
+            ApiPooler_FetcherHelper.run();
 
             // IgnoreMe file to internal screwing around
             //  Add a file named IgnoreMe.cs with a static void run() on it to play arround with the spider
