0cb5e195f4545bd16026685675d9e74ac43e6d6c
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-07-20 22:18:16 -0300

    Separated QuotesClass

---
+++ Simple.Test/Sample/QuotesClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Net.RafaelEstevam.Spider.Test.Sample
+{
+    public class QuotesObject
+    {
+        public bool has_next { get; set; }
+        public int page { get; set; }
+
+        public Quote[] quotes { get; set; }
+    }
+    public class Quote
+    {
+        public Author author { get; set; }
+        public string[] tags { get; set; }
+        public string text { get; set; }
+    }
+    public class Author
+    {
+        public string goodreads_link { get; set; }
+        public string name { get; set; }
+        public string slug { get; set; }
+    }
+}
--- Simple.Test/Sample/QuotesToScrape_Scroll_Deserialize.cs
+++ Simple.Test/Sample/QuotesToScrape_Scroll_Deserialize.cs
@@ -39,24 +39,5 @@ namespace Net.RafaelEstevam.Spider.Test.Sample
             return new Uri(url);
         }
 
-        public class QuotesObject
-        {
-            public bool has_next { get; set; }
-            public int page { get; set; }
-
-            public Quote[] quotes { get; set; }
-        }
-        public class Quote
-        {
-            public Author author { get; set; }
-            public string[] tags { get; set; }
-            public string text { get; set; }
-        }
-        public class Author
-        {
-            public string goodreads_link { get; set; }
-            public string name { get; set; }
-            public string slug { get; set; }
-        }
     }
 }
