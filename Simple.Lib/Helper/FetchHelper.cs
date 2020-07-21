c9dbe4b9ae76da9251a564abe85c089f037403ac
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-07-21 18:59:30 -0300

    Create FetchHelper.cs

---
+++ Simple.Lib/Helper/FetchHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Net.RafaelEstevam.Spider.Helper
+{
+    public static class FetchHelper
+    {
+        static WebClient wc;
+        static WebClient getClient()
+        {
+            if (wc == null) wc = new WebClient();
+            return wc;
+        }
+        /// <summary>
+        /// Fetch resource from uri
+        /// </summary>
+        /// <param name="uri">Uri to fetch from</param>
+        /// <returns>Byte aray with data fetched</returns>
+        public static byte[] FetchResource(Uri uri)
+        {
+            return getClient().DownloadData(uri);
+        }
+        /// <summary>
+        /// Fetch resource from uri
+        /// </summary>
+        /// <param name="uri">Uri to fetch from</param>
+        /// <returns>String with data fetched</returns>
+        public static string FetchResourceText(Uri uri, Encoding enc = null)
+        {
+            var data = getClient().DownloadData(uri);
+            //var contentType = getClient().ResponseHeaders[HttpResponseHeader.ContentType];
+            //if (!string.IsNullOrEmpty(contentType) && enc == null) { }
+            if (enc == null) enc = Encoding.UTF8;
+
+            return enc.GetString(data);
+        }
+    }
+}
