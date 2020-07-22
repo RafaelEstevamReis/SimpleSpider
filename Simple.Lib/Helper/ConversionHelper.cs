5e7e0e1b3a7aea98b65a3ad35100c848e85886ae
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-07-22 09:43:09 -0300

    Added ConversionHelper

---
+++ Simple.Lib/Helper/ConversionHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Net.RafaelEstevam.Spider.Helper
+{
+    public static class ConversionHelper
+    {
+        public static DateTime UnixEpoch(long Timestamp)
+        {
+            // I realy don't like it be a double ... ...
+            return DateTime.UnixEpoch.AddSeconds(Timestamp);
+        }
+        public static long UnixEpoch(DateTime dateTime)
+        {
+            // I realy don't like it be a double ... ...
+            return (int)(dateTime - DateTime.UnixEpoch).TotalSeconds;
+        }
+    }
+}
