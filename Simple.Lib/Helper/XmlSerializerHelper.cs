e5f937b9f2ee38fb3c875af31b3470eea012cd37
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-07-27 00:33:17 -0300

    Add XmlSerializerHelper.DeserializeFromFile

--- Simple.Lib/Helper/XmlSerializerHelper.cs
+++ Simple.Lib/Helper/XmlSerializerHelper.cs
@@ -5,11 +5,17 @@ namespace Net.RafaelEstevam.Spider.Helper
 {
     public static class XmlSerializerHelper
     {
-        public static void SerializeToFile<T>(T obj, string file)
+        public static void SerializeToFile<T>(T obj, string file) where T : new()
         {
             XmlSerializer x = new XmlSerializer(typeof(T));
             using TextWriter writer = new StreamWriter(file);
             x.Serialize(writer, obj);
+        }
+        public static T DeserializeFromFile<T>(string file) where T : new()
+        {
+            XmlSerializer x = new XmlSerializer(typeof(T));
+            using var reader = new StreamReader(file);
+            return (T)x.Deserialize(reader);
         }
     }
 }
