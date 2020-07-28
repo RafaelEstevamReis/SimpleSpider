96264b1f737bc328d2e896c3a02d47fb42b72de7
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-07-28 00:49:27 -0300

    Added more Configuration documentation

--- Simple.Lib/Configuration.cs
+++ Simple.Lib/Configuration.cs
@@ -2,6 +2,9 @@
 
 namespace Net.RafaelEstevam.Spider
 {
+    /// <summary>
+    /// Class to hold the configuration for the spider
+    /// </summary>
     public class Configuration
     {
         #region Internals SET
@@ -31,12 +34,17 @@ namespace Net.RafaelEstevam.Spider
         /// Are caching enabled? ICacher must support
         /// </summary>
         public bool Cache_Enable { get; set; } = true;
-
+        /// <summary>
+        /// Enables Caching using Cache_Enable property
+        /// </summary>
         public Configuration Enable_Caching()
         {
             Cache_Enable = true;
             return this; // Chaining
         }
+        /// <summary>
+        /// Disables Caching using Cache_Enable property
+        /// </summary>
         public Configuration Disable_Caching()
         {
             Cache_Enable = false;
@@ -47,12 +55,18 @@ namespace Net.RafaelEstevam.Spider
         /// How long cache files are valid?  ICacher must support
         /// </summary>
         public TimeSpan? Cache_Lifetime { get; set; }
-
+        /// <summary>
+        /// Sets cache limit
+        /// </summary>
+        /// <param name="timeSpan">Max age of the cache resource</param>
         public Configuration Set_CachingTTL(TimeSpan timeSpan)
         {
             Cache_Lifetime = timeSpan;
             return this; // Chaining
         }
+        /// <summary>
+        /// Sets cache limit to Infinity (no limit), using Cache_Lifetimeproperty set to null
+        /// </summary>
         public Configuration Set_CachingNoLimit()
         {
             Cache_Lifetime = null;
@@ -67,11 +81,19 @@ namespace Net.RafaelEstevam.Spider
         /// Time to wait in miliseconds between downloads
         /// </summary>
         public int DownloadDelay { get; set; } = 5000;
+        /// <summary>
+        /// Sets Downloader delay using DownloadDelay property
+        /// </summary>
+        /// <param name="Delay">Delay value</param>
         public Configuration Set_DownloadDelay(TimeSpan Delay)
         {
             DownloadDelay = (int)Delay.TotalMilliseconds;
             return this; // Chaining
         }
+        /// <summary>
+        /// Sets Downloader delay in milliseconds using DownloadDelay property
+        /// </summary>
+        /// <param name="Delay">Value in milliseconds</param>
         public Configuration Set_DownloadDelay(int Delay)
         {
             DownloadDelay = Delay;
@@ -82,11 +104,19 @@ namespace Net.RafaelEstevam.Spider
         /// Are cookies enabled? IDownloader must support
         /// </summary>
         public bool Cookies_Enable { get; set; } = false;
+        /// <summary>
+        /// Enables downloader to use Cookies, if supported. Uses Cookies_Enable property
+        /// </summary>
+        /// <returns></returns>
         public Configuration Enable_Cookies()
         {
             Cookies_Enable = true;
             return this; // Chaining
         }
+        /// <summary>
+        /// Disables downloader from using Cookies, if supported. Uses Cookies_Enable property
+        /// </summary>
+        /// <returns></returns>
         public Configuration Disable_Cookies()
         {
             Cookies_Enable = false;
@@ -118,12 +148,18 @@ namespace Net.RafaelEstevam.Spider
         /// Enable auto colelction of html Anchors tags
         /// </summary>
         public bool Auto_AnchorsLinks { get; set; } = true;
-
+        /// <summary>
+        /// Enable auto colelction of html Anchors tags, uses Auto_AnchorsLinks property
+        /// </summary>
         public Configuration Enable_AutoAnchorsLinks()
         {
             Auto_AnchorsLinks = true;
             return this; // Chaining
         }
+        /// <summary>
+        /// Disable auto colelction of html Anchors tags, uses Auto_AnchorsLinks property
+        /// </summary>
+        /// <returns></returns>
         public Configuration Disable_AutoAnchorsLinks()
         {
             Auto_AnchorsLinks = false;
