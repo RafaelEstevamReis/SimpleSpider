9a07e8163186825c539c771f60a6694ef39de7dd
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-07-29 13:55:17 -0300

    Document new UriHelpers

--- Simple.Lib/Helper/UriHelper.cs
+++ Simple.Lib/Helper/UriHelper.cs
@@ -23,25 +23,49 @@ namespace Net.RafaelEstevam.Spider.Helper
             return new Uri(parent, relative);
         }
 
+        /// <summary>
+        /// Returns if uri has a Fragment
+        /// </summary>
+        /// <param name="uri">Uri to check</param>
+        /// <returns>Returns true if it has a fragment, else otherwise</returns>
         public static bool HasFragment(this Uri uri)
         {
             return !string.IsNullOrEmpty(uri.Fragment);
         }
+        /// <summary>
+        /// Returns if uri has a Query
+        /// </summary>
+        /// <param name="uri">Uri to check</param>
+        /// <returns>Returns true if it has a query, else otherwise</returns>
         public static bool HasQuery(this Uri uri)
         {
             return !string.IsNullOrEmpty(uri.Query);
         }
-
+        /// <summary>
+        /// Removes the fragment from a Uri
+        /// </summary>
+        /// <param name="uri">Uri to remove from</param>
+        /// <returns>A new Uri without the fragment</returns>
         public static Uri RemoveFragment(this Uri uri)
         {
             if (!HasFragment(uri)) return uri;
             return new Uri(uri.ToString().Replace(uri.Fragment, ""));
         }
+        /// <summary>
+        /// Removes the query from a Uri
+        /// </summary>
+        /// <param name="uri">Uri to remove from</param>
+        /// <returns>A new Uri without the query</returns>
         public static Uri RemoveQuery(this Uri uri)
         {
             if (!HasQuery(uri)) return uri;
             return new Uri(uri.ToString().Replace(uri.Query, ""));
         }
+        /// <summary>
+        /// Removes the query and/or the fragment from a Uri
+        /// </summary>
+        /// <param name="uri">Uri to remove from</param>
+        /// <returns>A new Uri without the query and fragment</returns>
         public static Uri RemoveQueryAndFragment(this Uri uri)
         {
             if (!uri.HasFragment() && !uri.HasQuery()) return uri;
