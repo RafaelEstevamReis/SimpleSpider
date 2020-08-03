6be4d444f18190c13460eeff12a820cdcd0e5818
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-08-02 23:17:32 -0300

    Adds RequestHeaderExtension

---
+++ Simple.Lib/Extensions/RequestHeaderExtension.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Net.RafaelEstevam.Spider.Extensions
+{
+    /// <summary>
+    /// Request Headers extensions
+    /// </summary>
+    public static class RequestHeaderExtension
+    {
+        /// <summary>
+        /// Adds generic User-Agent, Accept-Language, Accept-Encoding, Accept, Dnt, and Upgrade-Insecure-Requests
+        /// </summary>
+        /// <param name="Header">Headers reference to add to</param>
+        public static void AddBaseRequestHeaders(this HeaderCollection Header)
+        {
+            // Accept-Language: en-GB,en-US;q=0.9,en;q=0.8
+            // Accept-Encoding: gzip, deflate
+            // Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*; q = 0.8
+            Header.AddItem("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36");
+            Header.AddItem("Accept-Language", "en-GB,en-US;q=0.9,en;q=0.8");
+            Header.AddItem("Accept-Encoding", "gzip, deflate");
+            Header.AddItem("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*; q = 0.8");
+            Header.AddItem("Dnt", "1");
+            Header.AddItem("Upgrade-Insecure-Requests", "1");
+        }
+        /// <summary>
+        /// Adds AddBaseRequestHeaders() then a firefox UA
+        /// </summary>
+        /// <param name="Header">Headers reference to add to</param>
+        public static void AddFirefoxRequestHeaders(this HeaderCollection Header)
+        {
+            AddBaseRequestHeaders(Header);
+            Header.AddItem("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0");
+        }
+        /// <summary>
+        /// Adds AddBaseRequestHeaders() then a Chrome Windows10 UA
+        /// </summary>
+        /// <param name="Header">Headers reference to add to</param>
+        public static void AddChromeW10RequestHeaders(this HeaderCollection Header)
+        {
+            AddBaseRequestHeaders(Header);
+            Header.AddItem("User-Agent", "Mozilla/5.0(Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.103 Safari/537.36");
+        }
+        /// <summary>
+        /// Adds AddBaseRequestHeaders() then a Chrome Linux X11 x64 UA
+        /// </summary>
+        /// <param name="Header">Headers reference to add to</param>
+        public static void AddChromeLinuxRequestHeaders(this HeaderCollection Header)
+        {
+            AddBaseRequestHeaders(Header);
+            Header.AddItem("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.103 Safari/537.36");
+        }
+    }
+}
