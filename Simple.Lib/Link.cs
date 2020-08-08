29bb2a83ab5f5be7aa68fbca474590fbcfc486e6
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-08-08 14:52:38 -0300

    Link Save/Load

--- Simple.Lib/Link.cs
+++ Simple.Lib/Link.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Net.RafaelEstevam.Spider
 {
@@ -7,7 +9,6 @@ namespace Net.RafaelEstevam.Spider
     /// </summary>
     public class Link
     {
-
         /// <summary>
         /// Primary Uri, the resource to be fetched
         /// </summary>
@@ -15,7 +16,7 @@ namespace Net.RafaelEstevam.Spider
         /// <summary>
         /// Uri where the Uri property was found
         /// </summary>
-        public Uri SourceUri { get; }
+        public Uri SourceUri { get; private set; }
         /// <summary>
         /// When redirected, the old Uri will be stored here
         /// </summary>
@@ -39,9 +40,11 @@ namespace Net.RafaelEstevam.Spider
         public TimeSpan FetchTime { get { return FetchEnd - FetchStart; } }
         
         /// <summary>
-        /// Specify and additional CallBack for this resource
+        /// Specify and additional CallBack for this resource. Can not be saved/Loaded
         /// </summary>
         public FetchComplete FetchCompleteCallBack { get; set; } = null;
+
+        private Link() { } // Empty, not safe
 
         /// <summary>
         /// Constructs a new Link
@@ -81,6 +84,75 @@ namespace Net.RafaelEstevam.Spider
         /// Implicit convertion from Link to Uri, returns Uri property
         /// </summary>
         public static implicit operator Uri(Link lnk) => lnk.Uri;
+        /// <summary>
+        /// Implicit convertion from Link to Url, returns Uri property
+        /// </summary>
+        public static implicit operator string(Link lnk) => lnk.Uri.ToString();
 
+        /// <summary>
+        /// Saves a link in a line-based http-like format
+        /// </summary>
+        /// <param name="link">Link to be saved</param>
+        /// <returns>Lines</returns>
+        public static IEnumerable<string> SaveLink(Link link)
+        {
+            yield return $"Uri: {link.Uri}";
+            yield return $"SourceUri: {link.SourceUri}";
+            if (link.MovedUri != null) yield return $"MovedUri: {link.MovedUri}";
+            if (link.RewrittenUri != null) yield return $"RewrittenUri: {link.RewrittenUri}";
+
+            yield return $"FetchStart: {link.FetchStart:yyyy-MM-ddTHH:mm:sszzz}";
+            yield return $"FetchEnd: {link.FetchEnd:yyyy-MM-ddTHH:mm:sszzz}";
+        }
+        /// <summary>
+        /// Loads a link from a line-based http-like format
+        /// </summary>
+        /// <param name="link">Lines to be saved</param>
+        /// <returns>Link</returns>
+        public static Link LoadLink(IEnumerable<string> content)
+        {
+            var lnk = new Link();
+
+            foreach (var line in content)
+            {
+                int colIdx = line.IndexOf(':');
+                if (colIdx <= 0) continue;
+
+                string key = line.Substring(0, colIdx).ToLower();
+                string value = line.Substring(colIdx);
+
+                switch (key)
+                {
+                    case "uri":
+                        lnk.Uri = new Uri(value);
+                        break;
+                    case "sourceuri":
+                        lnk.SourceUri = new Uri(value);
+                        break;
+
+                    case "moveduri":
+                        lnk.Uri = new Uri(value);
+                        break;
+                    case "rewrittenuri":
+                        lnk.RewrittenUri = new Uri(value);
+                        break;
+
+                    case "fetchstart":
+                        lnk.FetchStart = DateTime.Parse(value);
+                        break;
+                    case "fetchend":
+                        lnk.FetchEnd = DateTime.Parse(value);
+                        break;
+                }
+            }
+
+            if (lnk.Uri == null) throw new InvalidOperationException("Uri is not present");
+            if (lnk.SourceUri == null) throw new InvalidOperationException("SourceUri is not present");
+
+            if (lnk.FetchStart.Year < 2000) throw new InvalidOperationException("FetchStart is not present");
+            if (lnk.FetchStart.Year < 2000) throw new InvalidOperationException("FetchStart is not present");
+
+            return lnk;
+        }
     }
 }
