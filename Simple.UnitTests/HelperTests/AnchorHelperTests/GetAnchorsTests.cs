1dc139bebec3e0e9057879e00192ba90bea4d481
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-08-09 21:41:45 -0300

    Add tests for AnchorHelper.GetAnchors

---
+++ Simple.UnitTests/HelperTests/AnchorHelperTests/GetAnchorsTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Net.RafaelEstevam.Spider.Helper;
+using Xunit;
+
+namespace Net.RafaelEstevam.Spider.UnitTests.HelperTests.AnchorHelperTests
+{
+    public class GetAnchorsTests
+    {
+        [Fact]
+        public void AnchorHelper_GetAnchorsTests_Base()
+        {
+            var uri = new Uri("http://foo.bar");
+
+            string html = "<a href=\"TEST\"> test </a>";
+            var arr = AnchorHelper.GetAnchors(uri, html).ToArray();
+
+            Assert.Equal(new Uri[] { new Uri("http://foo.bar/TEST") }, arr);
+        }
+        [Fact]
+        public void AnchorHelper_GetAnchorsTests_MoreComplex()
+        {
+            var uri = new Uri("http://foo.bar");
+
+            string html = "<html><div><a></a><li><a href=\"TEST\"> test </a><a href=\"T2\"> test2 </a>";
+            var arr = AnchorHelper.GetAnchors(uri, html).ToArray();
+
+            Assert.Equal(new Uri[] 
+                         {
+                             new Uri("http://foo.bar/TEST"),
+                             new Uri("http://foo.bar/T2")
+                         },
+                         arr);
+        }
+        [Fact]
+        public void AnchorHelper_GetAnchorsTests_BrokenHtml_AnchorHRef()
+        {
+            var uri = new Uri("http://foo.bar");
+
+            string html = "<html><div><a></a><li><a ";
+            var arr = AnchorHelper.GetAnchors(uri, html).ToArray();
+
+            Assert.Empty(arr);
+        }
+        [Fact]
+        public void AnchorHelper_GetAnchorsTests_BrokenHtmlAnchor()
+        {
+            var uri = new Uri("http://foo.bar");
+
+            string html = "<html><div><a></a><li><a href=\"TEST\" id= \"A\"";
+            var arr = AnchorHelper.GetAnchors(uri, html).ToArray();
+
+            Assert.Empty(arr);
+        }
+        [Fact]
+        public void AnchorHelper_GetAnchorsTests_Empty()
+        {
+            var uri = new Uri("http://foo.bar");
+
+            string html = "<a id=\"TEST\"> test </a>";
+            var arr = AnchorHelper.GetAnchors(uri, html).ToArray();
+
+            Assert.Empty(arr);
+        }
+    }
+}
