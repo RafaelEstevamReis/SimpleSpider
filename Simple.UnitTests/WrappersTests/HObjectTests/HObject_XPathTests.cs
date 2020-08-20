c849f7dde7162cc20ae6f48554bbf0ab64d982a0
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-08-19 23:07:30 -0300

    Add HObject XPath selection tests

---
+++ Simple.UnitTests/WrappersTests/HObjectTests/HObject_XPathTests.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+
+namespace Net.RafaelEstevam.Spider.UnitTests.WrappersTests.HObjectTests
+{
+    public class HObject_XPathTests : HObjectTestBase
+    {
+        [Fact]
+        public void Wrappers_HObjectXPath_Base()
+        {
+            var h = GetHObject();
+            Assert.Equal("My First Heading", h.XPathSelect("//h1"));
+        }
+
+        [Fact]
+        public void Wrappers_HObjectXPath_ID()
+        {
+            var h = GetHObject();
+            Assert.Equal("nTxt2", h.XPathSelect("//input[@id=\"iTxt2\"]").GetNameValue());
+            Assert.Equal("My first paragraph.", h.XPathSelect("//div[@id=\"iTest1\"]/p"));
+        }
+
+        [Fact]
+        public void Wrappers_HObjectXPath_Class()
+        {
+            var h = GetHObject();
+            Assert.Equal("My first paragraph.", h.XPathSelect("//div[@class=\"cTest1\"]/p"));
+        }
+
+    }
+}
