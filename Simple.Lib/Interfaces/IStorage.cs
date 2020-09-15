6bd412383474ef0445908adb0145ad1726273b2d
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-09-15 11:38:02 -0300

    Adds IStorage

---
+++ Simple.Lib/Interfaces/IStorage.cs
@@ -0,0 +1,36 @@
+﻿namespace Net.RafaelEstevam.Spider.Interfaces
+{
+    /// <summary>
+    /// Represents a Interface to store collected data
+    /// </summary>
+    public interface IStorage
+    {
+        /// <summary>
+        /// Initialize the Storage with spider config
+        /// </summary>
+        void Initialize(Configuration Config);
+        /// <summary>
+        /// Instructs the storage to load saved data (optional)
+        /// </summary>
+        void LoadData();
+        /// <summary>
+        /// Instructs the storage to save data
+        /// </summary>
+        /// <param name="IsAutoSave">Indicates if was initiated by the auto-save</param>
+        void SaveData(bool IsAutoSave);
+
+        /// <summary>
+        /// Adds an item to the collection
+        /// </summary>
+        /// <param name="link">Link where the object was found</param>
+        /// <param name="item">Item to be collected</param>
+        /// <returns>Returns true if the item was added to the collection</returns>
+        bool AddItem(Link link, dynamic item);
+
+        /// <summary>
+        /// Countes the number of items collected
+        /// </summary>
+        /// <returns>Returns the number of items collected</returns>
+        int Count();
+    }
+}
