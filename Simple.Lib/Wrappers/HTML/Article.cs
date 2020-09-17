660f194dcacb961d9e002f99668a3ed3877e6701
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-09-16 23:26:18 -0300

    Add Html Article tag

---
+++ Simple.Lib/Wrappers/HTML/Article.cs
@@ -0,0 +1,19 @@
+﻿using HtmlAgilityPack;
+
+namespace Net.RafaelEstevam.Spider.Wrappers.HTML
+{
+    /// <summary>
+    /// Represents an html Article tag
+    /// </summary>
+    public class Article : Tag
+    {
+        /// <summary>
+        /// Initializes a new instance
+        /// </summary>
+        public Article(HtmlDocument doc) : base(doc) { ThrowsIfNotName(doc, "article"); }
+        /// <summary>
+        /// Initializes a new instance
+        /// </summary>
+        public Article(HtmlNode node) : base(node) { ThrowsIfNotName(node, "article"); }
+    }
+}
