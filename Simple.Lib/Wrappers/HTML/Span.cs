7f57d762aa07778fcb99eea3d3ba5f25542bfd4d
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-09-21 18:31:22 -0300

    Add HTML.Span

---
+++ Simple.Lib/Wrappers/HTML/Span.cs
@@ -0,0 +1,19 @@
+﻿using HtmlAgilityPack;
+
+namespace Net.RafaelEstevam.Spider.Wrappers.HTML
+{
+    /// <summary>
+    /// Represents an html Span tag
+    /// </summary>
+    public class Span : Tag
+    {
+        /// <summary>
+        /// Initializes a new instance
+        /// </summary>
+        public Span(HtmlDocument doc) : base(doc) { ThrowsIfNotName(doc, "span"); }
+        /// <summary>
+        /// Initializes a new instance
+        /// </summary>
+        public Span(HtmlNode node) : base(node) { ThrowsIfNotName(node, "span"); }
+    }
+}
