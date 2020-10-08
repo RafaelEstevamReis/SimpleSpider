c9cbab439eb9c524a6be1e9b5c23fd83ee275af5
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-10-07 22:25:56 -0300

    Add MissingTagsTests test

---
+++ Simple.UnitTests/WrappersTests/HtmlTests/MissingTagsTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Net.RafaelEstevam.Spider.Wrappers.HTML;
+using Xunit;
+
+namespace Net.RafaelEstevam.Spider.UnitTests.WrappersTests.HtmlTests
+{
+    public class MissingTagsTests
+    {
+        [Fact]
+        public void Wrappers_HtmlTag_TagListIsComplete()
+        {
+            // Check if all tags are specified
+
+            // Get all mapped tags types
+            var lstTags = Tag.MappingTable
+                .Select(o => o.Item2)
+                .OrderBy(o => o.Name)
+                .ToArray();
+            // Get all `Tag` implementations on assembly
+            var types = typeof(Tag).Assembly
+                .GetTypes()
+                .Where(t => !t.IsInterface)     // only te main classes
+                .Where(t => t != typeof(Tag))   // is not Tag itself
+                .Where(t => typeof(ITag).IsAssignableFrom(t))
+                .OrderBy(o => o.Name)
+                .ToArray();
+
+            // Check if booth collections are equal
+            Assert.Equal(types, lstTags);
+        }
+    }
+}
