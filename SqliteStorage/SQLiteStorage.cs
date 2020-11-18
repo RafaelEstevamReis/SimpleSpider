8f18acb5332daf7b3e82e4a5044bfe5f0e405467
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-11-18 18:22:39 -0300

    First Storage implementation

---
+++ SqliteStorage/SQLiteStorage.cs
@@ -0,0 +1,48 @@
+﻿using RafaelEstevam.Simple.Spider.Interfaces;
+using System;
+
+namespace RafaelEstevam.Simple.Spider.Storage
+{
+    public class SQLiteStorage<T> : IStorage
+        where T : new()
+    {
+        private readonly string tableNameT;
+        private Sqlite.Database db;
+
+        public SQLiteStorage()
+        {
+            tableNameT = typeof(T).Name;
+        }
+
+        public void Initialize(Configuration Config)
+        {
+            db = new Sqlite.Database(System.IO.Path.Combine(Config.SpiderDataDirectory.FullName,
+                                                            $"SqliteStorage_{tableNameT}.sqlite"));
+
+            db.CreateTables()
+              .Add<T>()
+              .Commit();
+        }
+
+        // There is no need to load
+        public void LoadData() { }
+        // There is no need to save
+        public void SaveData(bool IsAutoSave) { }
+
+        public bool AddItem(Link link, dynamic item)
+        {
+            return AddItem(link, (T)item);
+        }
+        public bool AddItem(Link link, T item)
+        {
+            // I'll not store the Link YET
+            db.Insert(item);
+            return true;
+        }
+
+        public int Count()
+        {
+            return db.ExecuteScalar<int>($"SELECT COUNT(*) FROM {tableNameT}", null);
+        }
+    }
+}
