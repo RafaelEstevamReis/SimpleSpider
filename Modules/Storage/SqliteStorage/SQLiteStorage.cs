e18b63ee57a06fab203447961e9501ee220d7616
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-11-20 22:10:33 -0300

    Add late-insert to SQLiteStorage

--- Modules/Storage/SqliteStorage/SQLiteStorage.cs
+++ Modules/Storage/SqliteStorage/SQLiteStorage.cs
@@ -1,19 +1,35 @@
 ﻿using RafaelEstevam.Simple.Spider.Interfaces;
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
 
 namespace RafaelEstevam.Simple.Spider.Storage
 {
     public class SQLiteStorage<T> : IStorage
         where T : new()
     {
+        List<(Link link, T item)> items;
+        Timer tmrInsertBlock;
+        object lockCommit;
+
         private readonly string tableNameT;
         private Sqlite.Database db;
 
         public SQLiteStorage()
         {
             tableNameT = typeof(T).Name;
+            items = new List<(Link, T)>();
+            lockCommit = new object();
+            tmrInsertBlock = new Timer()
+            {
+                AutoReset = false, // must be re-enabled after commit
+                Enabled = true,
+                Interval = 5 * 1000, // 5s 
+            };
+            tmrInsertBlock.Elapsed += TmrInsertBlock_Elapsed;
         }
-
         public void Initialize(Configuration Config)
         {
             db = new Sqlite.Database(System.IO.Path.Combine(Config.SpiderDataDirectory.FullName,
@@ -27,8 +43,10 @@ namespace RafaelEstevam.Simple.Spider.Storage
 
         // There is no need to load
         public void LoadData() { }
-        // There is no need to save
-        public void SaveData(bool IsAutoSave) { }
+        public void SaveData(bool IsAutoSave)
+        {
+            commitQueue();
+        }
 
         public bool AddItem(Link link, dynamic item)
         {
@@ -36,16 +54,53 @@ namespace RafaelEstevam.Simple.Spider.Storage
         }
         public bool AddItem(Link link, T item)
         {
-            // I'll not store the Link YET
-            var id = db.Insert(item);
-            db.Insert(new ObjectReference()
-            {
-                CrawTime = DateTime.Now,
-                Uri = link.Uri,
-                InsertedItem = id,
-            });
-
+            enqueue((link, item));
             return true;
+        }        
+
+        private void enqueue((Link link, T item) p)
+        {
+            items.Add(p);
+        }
+
+        private void TmrInsertBlock_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            commitQueue();
+        }
+
+        private void commitQueue()
+        {
+            if (items.Count == 0) return;
+            lock (lockCommit)
+            {
+                tmrInsertBlock.Enabled = false;
+
+                var array = items.ToArray();
+                items.Clear();
+
+                var ids = db.BulkInsert(array.Select(a => a.item));
+
+                var obs = new List<ObjectReference>();
+                for (int i = 0; i < ids.Length; i++)
+                {
+                    obs.Add(new ObjectReference()
+                    {
+                        InsertedItem = ids[i],
+                        CrawTime = DateTime.Now,
+                        Uri = array[i].link
+                    });
+                }
+                db.BulkInsert(obs);
+
+                if (array.Length != ids.Length)
+                {
+                }
+                if (array.Length != obs.Count)
+                {
+                }
+
+                tmrInsertBlock.Enabled = true;
+            }
         }
 
         public int Count()
