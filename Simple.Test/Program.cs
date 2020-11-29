ac5bf2456a671283f57796a99816f78fde3103f6
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-11-28 22:51:16 -0300

    Add more examples to Program

--- Simple.Test/Program.cs
+++ Simple.Test/Program.cs
@@ -6,23 +6,28 @@ namespace RafaelEstevam.Simple.Spider.Test
     {
         static void Main(string[] args)
         {
+            // See here some samples
+            // Uncomment and use the Debug Step Into feature (F11) to see the action
+
             // Spider Samples:
             //Sample.BooksToScrape.run();
-            //Sample.BooksToScrape_StorageJsonLines.run();
-            //Sample.QuotesToScrape_Scroll.run();
-            //Sample.QuotesToScrape_Scroll_Deserialize.run();
+            //Sample.QuotesToScrapeAPI_Scroll.run();
+            //Sample.QuotesToScrapeAPI_Scroll_Deserialize.run();
 
-            // Helper Samples, use the Debug Step Into feature (F11) to see the action
+            // Helper Samples
             //Sample.QuotesToScrape_Login.run();    // Capture a form element and submit it
             //Sample.ApiPooler_FetcherHelper.run(); // Easy fetch of single resources
 
             // Easy-parsing sample, use the Debug Step Into feature (F11) to see the action
             //Sample.QuotesToScrape_HObject.run(); // Parse quotes with HObject
 
+            //Storage Engines
+            //Sample.BooksToScrape_StorageJsonLines.run();
+            //ModulesSamples.Storage_Sqlite_Quotes.run();
+
             // IgnoreMe file to internal screwing around
             //  Add a file named IgnoreMe.cs with a static void run() on it to play around with the spider
             //IgnoreMe.run();
-
 
             SimpleSpider.HowToUse_PrintToConsole();
 
