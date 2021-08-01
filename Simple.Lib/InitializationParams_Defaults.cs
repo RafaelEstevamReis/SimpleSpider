7f038bc6d04001160ad711bbdf7dc90538988ee8
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-08-01 14:03:42 -0300

    Add Default003 - RequestCacher

--- Simple.Lib/InitializationParams_Defaults.cs
+++ Simple.Lib/InitializationParams_Defaults.cs
@@ -72,5 +72,28 @@ namespace RafaelEstevam.Simple.Spider
                                  .Set_DownloadDelay(DownloadDelay)
                                  .Enable_AutoAnchorsLinks());
         }
+
+        /// <summary>
+        /// Frozen in time default: RequestCacher, HttpClientDownloader with headers, NoLimitCaching, AutoAnchorsLinks enabled, and rewrites fragments
+        /// </summary>
+        /// <param name="DownloadDelay">Config.DownloadDelay in milliseconds</param>
+        public static InitializationParams Default003(int DownloadDelay = 5000)
+        {
+            // Have non-changing defaults helps with not breaking stuff
+            //but still have a good start point
+
+            return new InitializationParams()
+                // Set stable fetchers, future change in defaults
+                //will not affect this template
+                .SetCacher(new RequestCacher())
+                .SetDownloader(new HttpClientDownloader(true))
+                .SetStorage(null)
+                .SetConfig(c => c.Enable_AutoRewriteRemoveFragment()
+                                 .Enable_Caching()
+                                 .Set_CachingNoLimit()
+                                 .Disable_Cookies()
+                                 .Set_DownloadDelay(DownloadDelay)
+                                 .Enable_AutoAnchorsLinks());
+        }
     }
 }
