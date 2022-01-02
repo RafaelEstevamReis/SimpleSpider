1d02399aeb70244b3a82ab6e266642f4ed598db6
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-01-02 14:08:48 -0300

    Create IPageLinkCollector.cs

---
+++ Simple.Lib/Interfaces/IPageLinkCollector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RafaelEstevam.Simple.Spider.Interfaces
+{
+    public interface IPageLinkCollector
+    {
+        bool ExecuteFallBackIfError { get; }
+        bool CanProcessPage(Uri request, string htmlContent);
+        IEnumerable<Uri> GetLinks(Uri request, string htmlContent);
+        IPageLinkCollector FallBackProcessor { get; }
+    }
+}
